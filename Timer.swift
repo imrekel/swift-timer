bd87e85d5d885028165915413008254f86b2ad76
Alec Larson <alec.stanford.larson@gmail.com>
2014-07-24 07:36:56 -0700

    Update Timer.swift
    Cleaned things up. Anonymous timers now use `NSUUID().UUIDString` instead of my own `randomString()`. The `name` property is now a `String` instead of a `String?`.

--- Timer.swift
+++ Timer.swift
@@ -6,40 +6,19 @@ private var timers = [String:Timer]()
 
 public class Timer {
   
-  // Unique identifier for the Timer
-  public var name: String? {
-    didSet {
-      if oldValue == name { return }
-      if oldValue { timers[oldValue!] = nil }
-      if name { timers[name!] = self }
-    }
-  }
+  public let name: String
   
-  // How long to wait per delay cycle
   public let delay: Double
   
-  // What code to execute after every delay cycle
   public let callback: () -> ()
   
-  // If the Timer exceeds past one delay cycle
   public let repeats = false
   
-  /// Anonymous timer that's given a random name to be retained with
-  /// NB: Does NOT need to be manually retained
   public convenience init (_ delay: Double, _ callback: () -> ()) {
-    var name: String!
-    do {
-      name = __randomString(7)
-    } while timers.indexForKey(name)
-    
-    self.init(name, delay, callback)
+    self.init(NSUUID().UUIDString, delay, callback)
   }
   
-  /// Named timer that can be referenced via Timer.named("unique id")
-  /// NB: Does NOT need to be manually retained
   public init (_ name: String, _ delay: Double, _ callback: () -> ()) {
-  
-    // Stop a timer that has the same name, if it exists
     timers[name]?.kill()
     
     self.name = name
@@ -58,40 +37,32 @@ public class Timer {
     return Timer(repeat: delay, callback)
   }
   
-  // Returns an existing Timer or nil
   public class func named (name: String) -> Timer? {
     return timers[name]
   }
   
-  // Starts the Timer's delay cycle. 
-  // Automatically called in all Timer inits.
   public func start () {
-    if nsTimer { return } // Prevent the Timer from starting more than once
+    if nsTimer { return }
     nsTimer = NSTimer(timeInterval: NSTimeInterval(delay), target: self, selector: "execute", userInfo: nil, repeats: repeats)
     NSRunLoop.currentRunLoop().addTimer(nsTimer!, forMode: NSRunLoopCommonModes)
   }
   
-  // Pauses and resets the Timer's delay cycle
   public func pause () {
     if !nsTimer { return }
     nsTimer!.invalidate()
     nsTimer = nil
   }
   
-  // Immediately stops the Timer, and calls `self.callback`
   public func fire () {
     callback()
     kill()
   }
   
-  // Releases the Timer, causing it to be deallocated if you
-  // did not make a strong reference to it!
   public func kill () {
     pause()
-    name = nil
+    timers[name] = nil
   }
   
-  // The beef of the operation
   private var nsTimer: NSTimer?
   
   @objc private func execute () {
@@ -100,20 +71,7 @@ public class Timer {
   }
   
   private init (repeat delay: Double, _ callback: VoidClosure) {
-    self.delay = delay
-    self.callback = callback
-    self.repeats = true
-    
-    start()
+    repeats = true
+    self.init(delay, callback)
   }
-}
-
-private let __alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-
-private func __randomString (length: Int) -> String {
-  var string = ""
-  for _ in 0..<length {
-    string += __alphabet[Int(arc4random()) % countElements(__alphabet)]
-  }
-  return string
 }
